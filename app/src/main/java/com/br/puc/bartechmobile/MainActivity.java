6fe51ee75e62304af871336babb2d89981c36331
Pongelupe <pongelupe1998@gmail.com>
2018-11-25 20:14:45 -0200

    leitura do código de barra implementado

--- app/src/main/java/com/br/puc/bartechmobile/MainActivity.java
+++ app/src/main/java/com/br/puc/bartechmobile/MainActivity.java
@@ -1,13 +1,38 @@
 package com.br.puc.bartechmobile;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btScanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btScanner = findViewById(R.id.button);
+        btScanner.setOnClickListener(v -> {
+            Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+            intent.putExtra("ONE_D_MODE", "PRODUCT_MODE");//for Qr code, its "QR_CODE_MODE" instead of "PRODUCT_MODE"
+            intent.putExtra("SAVE_HISTORY", false);//this stops saving ur barcode in barcode scanner app's history
+            startActivityForResult(intent, 0);
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                String contents = data.getStringExtra("SCAN_RESULT");
+                System.out.println(contents);
+            } else if (resultCode == RESULT_CANCELED) {
+                // Handle cancel
+            }
+        }
+    }
+
 }
